58ad5f87798c135b7288ceb3e174d7ce681b5384
LisowskyNG <lisowsky@yandex.ru>
2024-03-29 19:21:54 +0100

    Task 1-3

--- src/Main.java
+++ src/Main.java
@@ -14,19 +14,49 @@ public class Main {
     public static void task1() {
         System.out.println("Задача 1");
         // Пишем код для задачи 1
-
+        int sum = 0;
+        int month = 0;
+        while (sum < 2459000){
+            sum = sum + 15000;
+            month++;
+            System.out.println("Месяц " +month+ " Сумма накоплений равна " +sum);
+        }
+        System.out.println();
     }
 
     public static void task2() {
         System.out.println("Задача 2");
         // Пишем код для задачи 2
+        int i = 1;
+        int j = 10;
+        while (i < 11) {
+            System.out.printf(i + " ");
+            i++;
+        }
+        System.out.println();
+        while (j > 0) {
+            System.out.printf(j + " ");
+            j--;
+        }
+        System.out.println();
+        System.out.println();
 
     }
 
     public static void task3() {
         System.out.println("Задача 3");
         // Пишем код для задачи 3
+        int population = 12000000;
+        for (int i = 1; i < 2; i++) {
+            int born = population / 1000 * 17;
+            int death = population / 1000 * 8;
+            population = population + born - death;
+            System.out.println("Год " +i+ " Численность населения " +population);
+//            System.out.println(born);
+//            System.out.println(death);
 
+        }
+        System.out.println();
     }
 
     public static void task4() {
